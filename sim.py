6a451b8f7392a0da4131288d8d5d8c0f2d673395
Etheiry <lateluffy@gmail.com>
2025-04-18 01:54:47 -0400

    ran simulation

--- sim.py
+++ sim.py
@@ -13,14 +13,15 @@ class Person:
         self.infected = False
         self.chanceToFindInfected = chanceToFindInfected
         self.infectionRate = infectionRate
-        self.seed_value = random.seed()
+        # self.seed_value = random.seed()
+        random.seed() # change seed value based of system time
         if group_type == "recovered":
             self.infectionRate = 0
         else:
             self.infectionRate = infectionRate
 
 
-
+    # Used by susceptible and recovered classes but recovered always returns True (win fight)
     def Fight(self):
         originalInfectionRate = infectionRate
         if self.has_weapon:
@@ -35,10 +36,13 @@ class Person:
             # Infected.count += 1
         # else:
         #     Infected.count -= 1                 # infected killed
-        #infectionRate = originalInfectionRate
-
-    def GatherResults(self, seed_value):
-        outcome = self.Forage(seed_value)
+        # infectionRate = originalInfectionRate
+        
+    # Susceptible and recovered both gather and can escape the island.
+    # Recovered has no need for weapons bc they win fight 100% of the time
+    # Vaccine can be used by both Susceptible and recovered
+    def Gather(self):
+        outcome = self.Forage()
        
         if outcome == 1:
             self.boat_found = True
@@ -47,26 +51,31 @@ class Person:
         elif outcome == 3:
             self.has_vaccine = True
          
-        print(f"Chance to find infected: {Susceptible.chanceToFindInfected}") # ! chanceToFindInfected not increasing correctly
+        # print(f"Chance to find infected: {self.chanceToFindInfected}") # ! chanceToFindInfected not increasing correctly
         if self.InfectedFound():
-            self.is_fighting = True
-        if self.boat_found == True and self.is_fighting == False:
-            self.Escape()
+            self.is_fighting = True # is fighting if infected found (recovered skip fighting state and instantly kill infected or 10% loss)
+        if self.boat_found == True and self.is_fighting == False: # can only escape if not fighting 
+            if self.group_type == "susceptible":
+                Escape(Susceptible)
+            else:
+                Escape(Recovered)
   
-
-    def Forage(self, seed_value):
-        outcome = random.seed(seed_value)
-        outcome = random.randint(1,4)    
-        return outcome
+    # Dice roll to determine gather results only 1 item can be found at a time
+    def Forage(self):
+        return random.randint(1,4)    
+        
     
+    # if roll is less than or equal to threshold infectedFound = True
     def InfectedFound(self):
         return random.randint(0, 100) <= self.chanceToFindInfected
     
 class Susceptible(Person): 
     group_count = 0   
-    def GiveVaccine():
-        if Infected.group_count > 0:
-            return True
+    def GiveVaccine(self):
+        if Infected.group_count > 0 and self.has_vaccine == True:
+            # return True
+            Infected.group_count -= 1
+            Recovered.group_count += 1
 
 
 class Recovered(Person):
@@ -83,8 +92,8 @@ class Dead():
 
 
 
-def Escape(self):
-    Susceptible.group_count -= 1
+def Escape(group):
+    group.group_count -= 1
     Escapee.group_count += 1
 
 
@@ -92,9 +101,10 @@ def Escape(self):
 
 
 def simulate():
+    # temp hard code values. 
     TOTAL_POPULATION = 50
     initial_susceptible = 45
-
+    step = 0
     susceptible_lst = []
     infected_lst = []
     recovered_lst = []
@@ -109,9 +119,23 @@ def simulate():
         Infected.group_count += 1
         infected_lst.append(Infected())
         
-
-    # print(Susceptible.group_count)
-    # print(Infected.group_count)
-
+    # Start simulation steps 
+    # while len(susceptible_lst) > 0 or len(recovered_lst) > 0:
+    while len(susceptible_lst) > 0:
+        step += 1
+        for sus in susceptible_lst:
+            sus.Gather()
+            sus.GiveVaccine()
+        for rec in recovered_lst:
+            rec.Gather()    
+        print("step " + str(step))
+        print("Susceptible count: " + str(Susceptible.group_count))
+        print("Recovered count: " + str(Recovered.group_count))
+        print("Infected count: " + str(Infected.group_count))
+        print("Dead count: " + str(Dead.group_count))
+    
+    
+    # logging
+    
 
 simulate()
